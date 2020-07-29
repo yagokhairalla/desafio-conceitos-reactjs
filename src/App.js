8bed1e315cfee5bfa52fcbb873d693835e7f33ca
Yago <kh.yago@gmail.com>
2020-07-29 18:59:43 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+
+  async function handleAddRepository() {    
+    const response = await api.post(`/repositories`, {
+      title: `New Repository ${Date.now()}`,
+      owner: 'default'      
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);    
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id));
   }
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => 
+            <li key={repository.id}>
+              {repository.title}
+  
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+          </li>
+        )}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
